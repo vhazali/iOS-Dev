5f0eedb3c892ed72339d26e7c71982dfcf2add2f
vhazali <victorhazali@gmail.com>
2015-10-18 15:05:14 +0800

    restarting calculator from scratch

---
+++ Calculator/Calculator/CalculatorModel.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorModel.swift
+//  Calculator
+//
+//  Created by Victor Hazali on 2/6/15.
+//  Copyright (c) 2015 Victor Hazali. All rights reserved.
+//
+
+import Foundation
---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Victor Hazali on 22/5/15.
+//  Copyright (c) 2015 Victor Hazali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
