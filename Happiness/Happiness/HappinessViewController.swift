d4e5c3bc2e35e5e79186488626c41c02eed8f30a
vhazali <victorhazali@gmail.com>
2015-10-20 09:31:44 +0800

    Initial Commit

---
+++ Happiness/Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Victor Hazali on 19/10/15.
+//  Copyright © 2015 Victor Hazali. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+}
